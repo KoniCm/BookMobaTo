dd1ab0387b7537fd02e92f360063f5e5676db79a
delga <ezgamer489@gmail.com>
2023-07-14 13:26:06 +0800

    My Realtime Data Book Management System Application Finished?

--- app/src/main/java/com/application/bookmobato/Librarian/BookListActivity.java
+++ app/src/main/java/com/application/bookmobato/Librarian/BookListActivity.java
@@ -27,6 +27,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import java.util.ArrayList;
 
 public class BookListActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
